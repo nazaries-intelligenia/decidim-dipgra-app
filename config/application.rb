7b1eb121eb3e82842891372d30f009d72dd2a808
Laura Jaime <laura.jv@coditramuntana.com>
2021-08-24 09:11:29 +0200

    Remove unnecesary files

--- config/application.rb
+++ config/application.rb
@@ -15,13 +15,5 @@ module DecidimCleanApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # initializer 'add named route overrides' do |app|
-    #   app.routes_reloader.paths << File.expand_path('../named_routes_overrides.rb',__FILE__)
-    #   # this seems to cause these extra routes to be loaded last, so they will define named routes last.
-    # end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
