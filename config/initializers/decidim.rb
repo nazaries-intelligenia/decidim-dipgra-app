0f4e92358287d260597f8421384d5f409a5f3fd0
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-04-12 08:11:35 +0200

    Use the same email in the mailer_sender as in he stmp config

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -3,7 +3,7 @@
 Decidim.configure do |config|
   config.application_name = "Decidim Clean APP"
 
-  config.mailer_sender = "decidim@decideix.com"
+  config.mailer_sender = Rails.application.secrets.smtp_username
 
   # Change these lines to set your preferred locales
   config.default_locale = :ca
