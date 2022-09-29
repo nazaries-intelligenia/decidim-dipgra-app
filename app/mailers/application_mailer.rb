e39f153d3347b729ab44ba97f267d857ed1bd0fb
LauraJaimeC <75725233+LauraJaimeC@users.noreply.github.com>
2022-09-29 13:16:34 +0200

    Set SMTP configuration (#5)
    * Set SMTP configuration
    
    * Set preprod env in secrets and username to figaro
    
    * Set default smtp settings in development env
    
    * Reset secrets

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: Rails.application.secrets.smtp_username
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,13 +74,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.smtp_address,
     port: Rails.application.secrets.smtp_port,
-    authentication: Rails.application.secrets.smtp_authentication,
-    user_name: Rails.application.secrets.smtp_username,
-    password: Rails.application.secrets.smtp_password,
     domain: Rails.application.secrets.smtp_domain,
-    enable_starttls_auto: Rails.application.secrets.smtp_starttls_auto,
-    openssl_verify_mode: "none",
-    ssl: true
+    ssl: false
   }
 
   if Rails.application.secrets.sendgrid
