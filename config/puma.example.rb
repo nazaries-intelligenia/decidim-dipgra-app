bfcb4fbe2edd8a4122334753200169f9c39f949c
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-11-15 19:06:47 +0100

    Update puma config

--- config/puma.example.rb
+++ config/puma.example.rb
@@ -1,22 +1,52 @@
 # frozen_string_literal: true
 
-workers Integer(ENV.fetch("WEB_CONCURRENCY", nil) || 1)
-threads_count = Integer(ENV.fetch("MAX_THREADS", nil) || 5)
-threads threads_count, threads_count
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
-# These make it crash on Heroku
-# pidfile "tmp/pids/puma.pid"
-# state_path "tmp/pids/puma.state"
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port ENV.fetch("PORT", 3000)
 
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV", "development")
+
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
+
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+workers ENV.fetch("WEB_CONCURRENCY", 1)
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
 preload_app!
 
-rackup DefaultRackup
-port ENV.fetch("PORT", nil) || 3000
-env = ENV.fetch("RACK_ENV", nil) || ENV.fetch("RAILS_ENV", nil) || :production
-environment env
+# If you are preloading your application and using Active Record, it's
+# recommended that you close any connections to the database before workers
+# are forked to prevent connection leakage.
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+end
 
+# The code in the `on_worker_boot` will be called if you are using
+# clustered mode by specifying a number of `workers`. After each worker
+# process is booted, this block will be run. If you are using the `preload_app!`
+# option, you will want to use this block to reconnect to any threads
+# or connections that may have been created at application boot, as Ruby
+# cannot share connections between processes.
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
