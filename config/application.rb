47ff7d97a17f1e4e87e0010cf553c6c876f6009c
Alejandro Rueda <alejandro.rueda@nazaries.com>
2024-06-14 11:24:46 +0200

    Upgrade to 0.28

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,13 @@
 require_relative "boot"
 
 require "decidim/rails"
+
 # Add the frameworks used by your app that are not loaded by Decidim.
-# require "action_cable/engine"
 # require "action_mailbox/engine"
 # require "action_text/engine"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
 
-require "net/http"
-require "openssl"
-require "resolv-replace"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -18,6 +17,14 @@ Bundler.require(*Rails.groups)
 module DecidimDipgraApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- decidim-dipgra_census_authorization/lib/decidim/dipgra_census_authorization/version.rb
+++ decidim-dipgra_census_authorization/lib/decidim/dipgra_census_authorization/version.rb
@@ -2,6 +2,6 @@
 
 module Decidim
   module DipgraCensusAuthorization
-    VERSION = "0.27.0"
+    VERSION = "0.28.0"
   end
 end
